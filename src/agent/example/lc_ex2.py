35a614cfe5f51168f95aa85e556796cde6dfa257
xjh <xjh@zjtuojing.com>
2025-07-04 19:47:34 +0800

    feat: llm-chatmodel

--- src/agent/example/lc_ex2.py
+++ src/agent/example/lc_ex2.py
@@ -1,13 +1,20 @@
 from langchain.chat_models import init_chat_model
 
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+API_KEY = config['openai']['api_key']
+BASE_URL = config['openai']['base_url']
+MODEL = config['openai']['model']
+
 messages = [
     {"role": "user", "content": "介绍下你自己"}
 ]
 
 
-model = init_chat_model("deepseek-chat",
+model = init_chat_model(MODEL,
  model_provider="openai",
-  openai_api_key="sk-edeb86a2315f496cabe6fa61ed0b2af6",
-   openai_api_base="https://api.deepseek.com/v1")
+  openai_api_key=API_KEY,
+   openai_api_base=BASE_URL)
 
 print(model.invoke(messages).content)
